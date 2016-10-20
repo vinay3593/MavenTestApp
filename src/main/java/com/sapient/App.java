0dc345b9e35da567bf28172542d6bbd7584d021f
Vinay Kumar <vkum64@sapient.com>
2016-10-20 15:23:08 +0530

    Maven Initial Commit

---
+++ src/main/java/com/sapient/App.java
@@ -0,0 +1,17 @@
+package com.sapient;
+import org.slf4j.*;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	Logger logger = LoggerFactory.getLogger(App.class);
+	logger.info("Hello World");
+    }
+}
---
+++ src/test/java/com/sapient/AppTest.java
@@ -0,0 +1,50 @@
+package com.sapient;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp1()
+    {
+        assertTrue( true );
+    }
+	public void testApp2()
+    {
+        assertTrue( true );
+    }
+	public void testApp3()
+    {
+        assertTrue( true );
+    }
+	public void testApp4()
+    {
+        assertTrue( true );
+    }
+}
